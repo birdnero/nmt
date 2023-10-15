3e3be7e8d459945817769536a56ba679baa007b9
birdnero <neroforwork@gmail.com>
2023-10-16 00:13:50 +0300

    fix problems #2

--- src/components/components/settings.tsx
+++ src/components/components/settings.tsx
@@ -2,7 +2,6 @@ import { Space } from 'antd'
 import '../diary.scss'
 import Object from './object'
 import Theme from './theme'
-import DeleteResults from './deleteResults'
 
 const Settings: React.FC = () => {
     
